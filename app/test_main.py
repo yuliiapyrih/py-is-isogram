57cb1b66f898263a227ac51503f19598f7743606
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-11-02 00:21:49 +0200

    implement py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,82 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, expected_result",
+        [
+            pytest.param(
+                "",
+                True,
+                id="should return True "
+                   "if word is empty"
+            ),
+            pytest.param(
+                "playgrounds",
+                True,
+                id="should return True "
+                   "if string is isogram"
+            ),
+            pytest.param(
+                "House",
+                True,
+                id="should return True "
+                   "if string is isogram has large letter"
+            ),
+            pytest.param(
+                "FAMILY",
+                True,
+                id="should return True "
+                   "if string is isogram has only large letters"
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="should return False "
+                   "if string is not isogram"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="should return False "
+                   "if string is not isogram has large letter"
+            ),
+            pytest.param(
+                "BABY",
+                False,
+                id="should return False "
+                   "if string is not isogram has only large letters"
+            ),
+        ]
+    )
+    def test_is_isogram(
+            self,
+            word: str,
+            expected_result: bool
+    ) -> None:
+        assert is_isogram(word) == expected_result
+
+    @pytest.mark.parametrize(
+        "word, expected_error",
+        [
+            pytest.param(
+                18,
+                AttributeError,
+                id="should raise error for integer value"
+            ),
+            pytest.param(
+                None,
+                AttributeError,
+                id="should raise error for None types"
+            ),
+        ]
+    )
+    def test_is_isogram_exceptions(
+            self,
+            word: int | None,
+            expected_error: Exception
+    ) -> None:
+        with pytest.raises(expected_error):
+            is_isogram(word)
